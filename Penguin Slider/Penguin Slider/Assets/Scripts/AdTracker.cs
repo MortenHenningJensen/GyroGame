c294f1b40a68ae9267ec2f8574c10f326b1ee42f
Morten Jensen <mortenhenningjensen@hotmail.com>
2017-08-19 18:46:41 +0200

    changed project name started on ads made particle system when moving

---
+++ Penguin Slider/Penguin Slider/Assets/Scripts/AdTracker.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdTracker : MonoBehaviour {
+
+    private static AdTracker instanceRef;
+    public int attemptCounter;
+
+    // Use this for initialization
+    void Start () {
+
+        if (instanceRef == null)
+        {
+            instanceRef = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            DestroyImmediate(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (attemptCounter >= 5)
+        {
+            Advertisement.Show();
+            attemptCounter = 0;
+        }
+	}
+}
---
+++ Penguin Slider/Penguin Slider/Assets/Scripts/GameEndScreen.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEndScreen : MonoBehaviour
+{
+    public bool levelCompleted;
+   // private GameControl control;
+    public int starsUnlocked;
+    private AdTracker at;
+
+    private void Start()
+    {
+        at = GameObject.FindGameObjectWithTag("AdTracker").GetComponent<AdTracker>();
+        levelCompleted = false;
+    //    control = FindObjectOfType<GameControl>();
+    }
+
+    public void BackToMenu()
+    {
+        if (Time.timeScale != 1)
+        {
+            Time.timeScale = 1;
+        }
+        Destroy(GameObject.Find("GameTracker"));
+
+        if (levelCompleted)
+        {
+            SaveProgress(SceneManager.GetActiveScene().name.ToString(), starsUnlocked);
+        }
+
+        at.attemptCounter++;
+        SceneManager.LoadScene("Level Select");
+    }
+
+    public void ReplayLevel()
+    {
+        if (Time.timeScale != 1)
+        {
+            Time.timeScale = 1;
+        }
+        Destroy(GameObject.Find("GameTracker"));
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+        if (levelCompleted)
+        {
+            SaveProgress(SceneManager.GetActiveScene().name.ToString(), starsUnlocked);
+        }
+        at.attemptCounter++;
+    }
+
+    public void SaveProgress(string level, int stars)
+    {
+        PlayerPrefs.SetInt("Level " + level, stars);
+        PlayerPrefs.SetInt("lvl " + level, 1);
+
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+
+        string path = SceneUtility.GetScenePathByBuildIndex(nextSceneIndex);
+        string sceneName = path.Substring(0, path.Length - 6).Substring(path.LastIndexOf('/') + 1);
+
+        PlayerPrefs.SetInt("lvl " + sceneName, 1);
+    }
+}
