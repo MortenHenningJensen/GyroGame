9c18df38d9e9b07130c92e38955de9ae310460fe
Morten Jensen <mortenhenningjensen@hotmail.com>
2017-08-21 23:18:15 +0200

    Set up level select for multiple "worlds" Made more sprites for images in sand world

--- Penguin Slider/Penguin Slider/Assets/Scripts/MainMenu.cs
+++ Penguin Slider/Penguin Slider/Assets/Scripts/MainMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
@@ -10,7 +11,14 @@ public class MainMenu : MonoBehaviour
     public Canvas worldTwo;
     public Canvas worldThree;
     public Canvas worldFour;
+    public Canvas worldSelectIce;
+    public Canvas worldSelectSand;
     public Canvas worldSelect;
+
+    public Canvas worldFive;
+    public Canvas worldSix;
+    public Canvas worldSeven;
+    public Canvas worldEight;
 
     // Use this for initialization
     void Start()
@@ -26,35 +34,79 @@ public class MainMenu : MonoBehaviour
 
     }
 
+#region Ice Levels
+
     public void EnterWorldOne()
     {
         worldOne.enabled = true;
 
-        worldSelect.enabled = false;
+        worldSelectIce.enabled = false;
     }
 
     public void EnterWorldTwo()
     {
         worldTwo.enabled = true;
 
-        worldSelect.enabled = false;
+        worldSelectIce.enabled = false;
     }
 
     public void EnterWorldThree()
     {
         worldThree.enabled = true;
 
-        worldSelect.enabled = false;
+        worldSelectIce.enabled = false;
     }
 
     public void EnterWorldFour()
     {
         worldFour.enabled = true;
 
+        worldSelectIce.enabled = false;
+    }
+
+#endregion
+
+    public void EnterWorldFive()
+    {
+        worldFive.enabled = true;
+
+        worldSelectSand.enabled = false;
+    }
+
+    public void EnterWorldSix()
+    {
+        worldSix.enabled = true;
+
+        worldSelectSand.enabled = false;
+    }
+
+    public void EnterWorldSeven()
+    {
+        worldSeven.enabled = true;
+
+        worldSelectSand.enabled = false;
+    }
+
+    public void EnterWorldEight()
+    {
+        worldEight.enabled = true;
+
+        worldSelectSand.enabled = false;
+    }
+
+    public void EnterIceWorlds()
+    {
+        worldSelectIce.enabled = true;
+
         worldSelect.enabled = false;
     }
 
+    public void EnterSandWorlds()
+    {
+        worldSelectSand.enabled = true;
 
+        worldSelect.enabled = false;
+    }
 
     public void BackButton()
     {
@@ -63,7 +115,29 @@ public class MainMenu : MonoBehaviour
         worldThree.enabled = false;
         worldFour.enabled = false;
 
+        worldSelectIce.enabled = true;
+    }
+
+    public void BackButtonSand()
+    {
+        worldFive.enabled = false;
+        worldSix.enabled = false;
+        worldSeven.enabled = false;
+        worldEight.enabled = false;
+
+        worldSelectSand.enabled = true;
+    }
+
+    public void BackToWorldSelect()
+    {
+        worldSelectIce.enabled = false;
+        worldSelectSand.enabled = false;
+
         worldSelect.enabled = true;
     }
 
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
